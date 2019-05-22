47c4b5fa37fbd7b8a212025f8051c071dac9e490
TheLKL321 <lkl.zarebski@gmail.com>
2019-05-22 23:13:27 +0200

    record keeping functionality

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+const PORT = 7100;
+const URL = "http://localhost:" + PORT;
+const RECORD_LIST_LENGTH = 10;
+
 const express = require('express');
 const app = express();
-const port = 7001;
-app.get('/', function (request, response) {
-    response.send("pong");
+app.use(express.json());
+
+let records = [{ nickname: "lulz", taps: 1337, timestamp: "13/02 1:37:42"}];
+
+app.get('/', (request, response) => {
+    response.send(JSON.stringify(records));
 });
-app.listen(port, function () {
-    console.log('Server listening on http://localhost:' + port);
+
+app.post('/addRecord', (request, response) => {
+    console.log("Received an entry:\n" + request.body);
+
+    let score = Number(request.body.taps);
+    let record = {
+        nickname: request.body.nickname,
+        taps: Number(request.body.taps),
+        timestamp: request.body.timestamp
+    };
+    if (records.length < RECORD_LIST_LENGTH) {
+        records.push(record);
+    } else if (score >= records[RECORD_LIST_LENGTH - 1].taps) {
+        records[records.findIndex((element, index, array) => {
+            return element === array[RECORD_LIST_LENGTH - 1];
+        })] = record;
+    }
+    records.sort((a, b) => { return a.taps - b.taps });
+
+    response.send("Received");
+});
+
+app.listen(PORT, () => {
+    console.log('Server listening on ' + URL);
 });
